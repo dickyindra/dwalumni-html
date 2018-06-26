2b20c0d8d2c6257438e4c06ab4462405c2d9a578
HandiSutriyan <handisutrian@gmail.com>
2018-06-26 11:36:49 +0700

    tambah halaman detail

--- pages/profil/index.php
+++ pages/profil/index.php
@@ -1,9 +1,22 @@
-<?php
-	$pages = [
+
+<link rel="stylesheet" href="pages/profil/style.css" >
+
+<div class="col-md-8">
+
+<div class="container border border-light z-depth-1 mr-md-auto" style="margin: 20px 0 20px 0;">
+<ul class="nav nav-tabs" style="margin: 20px 0 0 0;">
+    <a class="nav-link active-tab" href="#"><i class="fab fa-react"></i> React Native</a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link inactive-tab" href="#" ><i class="fab fa-python"></i> Python</a>
+  </li>
+</ul>
 		
-		"tab-profil",
-		"exam-result",
-		"articles"
-	];
-	IncludePage($pages)
+		<?php include "components/tab-profil/index.html";?>
+		<?php include "components/exam-result/index.html";?>
+		<?php include "components/articles/index.html";?>
+		<?php include "components/certificates/index.html";?>
+	
 ?>
+</div>
+</div>
